bb61b0188b7edd94d498eb2d2b00aca9fc72c16f
artronics <jalalhosseiny@gmail.com>
2023-07-19 21:34:34 +0100

    add bare fuzzy second attempt

---
+++ proto/fuzzy_score_2.py
@@ -0,0 +1,11 @@
+from random import randint
+
+from run_search import run_search
+
+
+def fuzzy_search_2(text: str, pattern: str):
+    return randint(-100000, 100000)
+
+
+if __name__ == '__main__':
+    run_search(fuzzy_search_2)
